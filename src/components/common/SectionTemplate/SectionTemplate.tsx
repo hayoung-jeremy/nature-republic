9bbcc4fc7fcce8e11813dc6b411e44d5c5c8a173
Ha young, Kim <jeremiah91@naver.com>
2021-06-21 16:12:57 +0900

    feat : Add SectionTemplate component

---
+++ src/components/common/SectionTemplate/SectionTemplate.tsx
@@ -0,0 +1,50 @@
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+
+const StyledSectionTemplate = styled.section`
+  & h2.blind {
+    position: absolute;
+    left: -9999rem;
+    width: 1px;
+    height: 1px;
+    clip: rect(0 0 0 0);
+    overflow: hidden;
+  }
+  & h2.title {
+    font-size: 20px;
+  }
+  & .inner {
+    width: 1200px;
+    margin: 0 auto;
+  }
+`;
+
+interface SectionProps {
+  children: ReactNode;
+  hasInner?: boolean;
+  isH2Blind?: boolean;
+  h2Content: string;
+}
+
+const SectionTemplate = ({
+  children,
+  hasInner,
+  isH2Blind,
+  h2Content,
+  ...rest
+}: SectionProps) => {
+  return (
+    <StyledSectionTemplate>
+      {hasInner ? (
+        <div className="inner">
+          <h2 className={isH2Blind ? "blind" : "title"}>{h2Content}</h2>
+          {children}
+        </div>
+      ) : (
+        children
+      )}
+    </StyledSectionTemplate>
+  );
+};
+
+export default SectionTemplate;
