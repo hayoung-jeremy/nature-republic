9c4a924aafdaa2a3d7ea25e2f8da3e4b628d9dcb
Ha young, Kim <jeremiah91@naver.com>
2021-06-27 16:06:42 +0900

    fix : Fix the swiper error --trial-03  - had to use Swiper directly

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,3 @@ export { default as PageTemplate } from "components/page";
 export { default as Header } from "components/header";
 export { default as Footer } from "components/footer";
 export { default as SectionTemplate } from "components/section";
-export { default as SwiperTemplate } from "components/swiper";
--- src/containers/Home/HomeContainer.tsx
+++ src/containers/Home/HomeContainer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 // swiper :
 import SwiperCore, { Navigation, Pagination, Virtual } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -9,7 +8,6 @@ import "swiper/components/pagination/pagination.scss";
 // custom :
 import bannerImages from "db/bannerImages.json";
 import { SectionTemplate } from "components";
-import { SwiperTemplate } from "components";
 
 SwiperCore.use([Virtual, Navigation, Pagination]);
 
@@ -32,15 +30,10 @@ const HomeContainer = () => {
           })}
         </Swiper>
       </SectionTemplate>
-      <SectionTemplate hasInner h2Content="신제품 & 베스트셀러">
-        <SwiperTemplate
-          data={bannerImages}
-          slidesPerView={1}
-          virtual
-          pagination={{ clickable: true }}
-          navigation
-        />
-      </SectionTemplate>
+      <SectionTemplate
+        hasInner
+        h2Content="신제품 & 베스트셀러"
+      ></SectionTemplate>
       <SectionTemplate hasInner h2Content="추천 카테고리"></SectionTemplate>
       <SectionTemplate hasInner h2Content="EVENT"></SectionTemplate>
       <SectionTemplate hasInner h2Content="Best Reviews"></SectionTemplate>
