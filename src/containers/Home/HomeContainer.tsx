90a5fb04f45fb3862b1ac259c47f06b70f15c14e
Ha young, Kim <jeremiah91@naver.com>
2021-07-03 22:02:11 +0900

    feat : Refactoring section

--- src/containers/Home/HomeContainer.tsx
+++ src/containers/Home/HomeContainer.tsx
@@ -1,13 +1,14 @@
 // custom :
 import { SectionTemplate } from "components";
 import { MainBannerContainer, NewAndBestContainer } from "containers";
+import { RecommendedContainer } from "containers";
 
 const HomeContainer = () => {
   return (
     <>
       <MainBannerContainer />
       <NewAndBestContainer />
-      <SectionTemplate hasInner h2Content="추천 카테고리"></SectionTemplate>
+      <RecommendedContainer />
       <SectionTemplate hasInner h2Content="EVENT"></SectionTemplate>
       <SectionTemplate hasInner h2Content="Best Reviews"></SectionTemplate>
     </>
---
+++ src/containers/Section/Recommended/RecommendedContainer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SectionTemplate } from "components";
+
+const RecommendedContainer = () => {
+  return (
+    <SectionTemplate hasInner h2Content="추천 카테고리" grayBackground>
+      <div>카테고리 메뉴</div>
+      <div>Swiper</div>
+    </SectionTemplate>
+  );
+};
+
+export default RecommendedContainer;
